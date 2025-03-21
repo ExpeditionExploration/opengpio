ffeec9067ac75541f601042342600b1108b298af
Kyle <kyle.ueckermann@gmail.com>
2025-03-22 01:23:04 +0200

    renamed for consistency

--- src/bindings.ts
+++ src/bindings.ts
@@ -1,16 +1,16 @@
 import binding from 'bindings';
 import { type OpenGpioBindings } from './types';
-import debug from './debug';
-const logger = debug.extend('bindings');
+import { default as debugLogger } from './debug';
+const debug = debugLogger.extend('bindings');
 
 let bindings: OpenGpioBindings = {} as any;
 
 const mocked = process.env.OPENGPIO_MOCKED === 'true';
 if (!mocked) {
-    logger('Loading bindings...');
+    debug('Loading bindings...');
     bindings = binding('opengpio');
 } else {
-    logger('Using mocked bindings...');
+    debug('Using mocked bindings...');
     bindings.info = () => 'mocked';
     bindings.input = () => [() => true, () => { }];
     bindings.output = () => [() => { }, () => { }];
