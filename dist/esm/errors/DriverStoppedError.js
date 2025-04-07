2e3d5f2d4a1e5e26cc67edda7bf87673347ef0c8
Kyle <kyle.ueckermann@gmail.com>
2025-04-08 00:00:38 +0300

    test

---
+++ dist/esm/errors/DriverStoppedError.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DriverStoppedError = void 0;
+class DriverStoppedError extends Error {
+}
+exports.DriverStoppedError = DriverStoppedError;
